3400793eca481030ba403a8849e38a5539dccd79
adavydov1 <63337035+adavydov1@users.noreply.github.com>
2022-05-04 17:43:08 +0300

    Add files via upload

---
+++ davydovTask/davydovTask/Controllers/SignInViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  davydovTask
+//
+//  Created by Student on 04.05.2022.
+//
+
+import UIKit
+
+class SignInViewController: UIViewController {
+
+    @IBOutlet weak var inputLogin: UITextField!
+    @IBOutlet weak var inputPassword: UITextField!
+    
+    @IBAction func signInAction( sender: UIButton) {
+        
+    }
+
+}
+
